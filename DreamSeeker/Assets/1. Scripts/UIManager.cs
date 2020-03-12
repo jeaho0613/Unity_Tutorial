1aec4cfee2e1969c10d7fac90fd381812c842e31
jeaho0613 <jeaho0613@gmail.com>
2020-03-12 21:45:49 +0900

    Intro 구현중.
    - 시네머신을 활용한 카메라 제어

---
+++ DreamSeeker/Assets/1. Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public Text text1;
+    public Text text2;
+    
+    void Start()
+    {
+        text1.DOText("Jae Ho Games #1", 5, true, ScrambleMode.All)
+            .OnStepComplete(() =>
+            {
+                text2.DOText("Unity.", 2, true).SetAutoKill(false);
+                text2.DOColor(RandomColor(), 1.5f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Flash);
+            });
+    }
+
+    Color RandomColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
+    }
+}
+
