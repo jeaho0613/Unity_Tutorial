3a82296ab6be1d37a53cd9e041a18786969caea0
jeaho0613 <jeaho0613@gmail.com>
2020-03-14 17:13:19 +0900

    Intro Scene 완성
    - DOTween을 활용한 애니메이션 구성

--- DreamSeeker/Assets/1. Scripts/UIManager.cs
+++ DreamSeeker/Assets/1. Scripts/UIManager.cs
@@ -1,21 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
+    // Ani 효과 텍스트
     public Text text1;
     public Text text2;
-    
+
+    // Fade 효과 변수
+    public SpriteRenderer fade;
+
+    // sound 
+    public AudioSource typing;
+
     void Start()
     {
-        text1.DOText("Jae Ho Games #1", 5, true, ScrambleMode.All)
+        StartCoroutine(Text1());
+        Invoke("Text2", 5);
+    }
+    
+    IEnumerator Text1()
+    {
+        typing.PlayDelayed(0.5f);
+        text1.DOText("[ jae ho games ]", 4, true, ScrambleMode.All);
+        yield return new WaitForSeconds(3f);
+        typing.Stop();
+    }
+
+    void Text2()
+    {
+        text2.DOText("#1", 1, true).SetEase(Ease.Linear);
+        text2.DOColor(RandomColor(), 1.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear)
             .OnStepComplete(() =>
             {
-                text2.DOText("Unity.", 2, true).SetAutoKill(false);
-                text2.DOColor(RandomColor(), 1.5f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Flash);
+                fade.DOFade(1, 5);
             });
     }
 
